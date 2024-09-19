a21b45af04ffa7b6be8d669952e01c08079d5e79
Dave Peterson <davpeterson@zoho.com>
2024-09-19 15:34:36 -0700

    read-file

--- app/api/readshows/route.ts
+++ app/api/readshows/route.ts
@@ -1,15 +1,31 @@
 
 import conn from '@/lib/db';
-import fs from 'fs'
+import fs from 'fs';
+import fsp from 'fs/promises';
 import path from 'path'
 import { NextResponse } from 'next/server'
 
+const PATH = './public/files/';
+	
 export async function GET(request: Request): Response{
+	console.warn('readShowFiles');
 	try{
-		console.warn('readShowFiles');
 		const filenames = await getFilenames();
 		console.warn('filenames', filenames);
-		return NextResponse.json(filenames);
+		
+		//TODO: remove all shows from DB
+		let fileContents = '>nothing<';
+		for(const filename of filenames){
+			console.warn('filename', filename);
+			fileContents = await readFile(filename);
+			console.warn('fileContents.toString', fileContents);
+			
+			break;
+		}
+			
+		
+		
+		return NextResponse.json(fileContents);
 	}catch(error){
 		console.error('FileSystem Error:', error);
 		throw new Error('Failed to retrieve show filenames.');
@@ -17,11 +33,15 @@ export async function GET(request: Request): Response{
 }
 
 async function getFilenames(){
-	console.warn('readShowFiles');
-	const PATH = './public/files/';
 	const dir = path.resolve(PATH);
 	console.warn('dir', dir);
 	const filenames = await fs.readdirSync(dir);
-	console.warn('filenames', filenames);
+//	console.warn('filenames', filenames);
 	return filenames;
+}
+
+async function readFile(filename){
+	const fileContents = await fsp.readFile(PATH + filename);
+	console.warn('fileContents', fileContents);
+	return fileContents.toString();
 }
