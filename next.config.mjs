c3176dc58eabe801af1cffcc6b81464f5e767adf
Dave Peterson <davpeterson@zoho.com>
2026-03-30 14:22:36 -0700

    fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
-// eslint.config.mjs
-import js from "@eslint/js";
-
-export default [
-  js.configs.recommended,
-  {
-    ignores: [".next/**", "node_modules/**"],
-    rules: {
-      "no-unused-vars": "off",
-      "no-undef": "off",
-      "react/react-in-jsx-scope": "off",
-    },
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-];
+
+  outputFileTracingExcludes: {
+    '**/*': [
+      './public/music/**/*',
+      './public/files/**/*',
+    ],
+  },
+
+  outputFileTracingRoot: process.cwd(),
+};
+
+export default nextConfig;
